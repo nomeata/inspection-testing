b399cab76d966250dfe6fe9b76b1a8451b4c2449
Joachim Breitner <mail@joachim-breitner.de>
2017-04-27 09:34:16 -0400

    Introduce a type alias for Proofs

--- GHC/Proof.hs
+++ GHC/Proof.hs
@@ -1,18 +1,20 @@
 module GHC.Proof where
 
-proof :: a -> a -> ()
-proof _ _ = ()
+data Proof = Proof
+
+proof :: a -> a -> Proof
+proof _ _ = Proof
 {-# INLINE [0] proof #-}
 
-(===) :: a -> a -> ()
+(===) :: a -> a -> Proof
 (===) = proof
 infix 0 ===
 
-non_proof :: a -> a -> ()
-non_proof _ _ = ()
+non_proof :: a -> a -> Proof
+non_proof _ _ = Proof
 {-# INLINE [0] non_proof #-}
 
-(=/=) :: a -> a -> ()
+(=/=) :: a -> a -> Proof
 (=/=) = non_proof
 infix 0 =/=
 
