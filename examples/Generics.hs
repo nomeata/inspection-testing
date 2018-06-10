b3782449790628fcd1bf0edc3cff1d51d99c1972
Sandy Maguire <sandy@sandymaguire.me>
2018-06-10 13:15:33 +0200

    joachim wants tests so tests he gets

---
+++ examples/Generics.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE RankNTypes, DeriveGeneric, TypeApplications, DataKinds, ExistentialQuantification, TemplateHaskell #-}
+{-# OPTIONS_GHC -O -fplugin Test.Inspection.Plugin #-}
+module Generics (main) where
+
+import GHC.Generics
+import Test.Inspection
+
+data Record = MkRecord
+  { fieldA :: Int
+  , fieldB :: Bool
+  } deriving Generic
+
+
+myRecord :: Record
+myRecord = MkRecord 1 True
+
+genericRep :: Rep Record x
+genericRep = from myRecord
+
+
+roundTripRep :: Record
+roundTripRep = to $ from myRecord
+
+main :: IO ()
+main = return ()
+
+-- the check
+inspect $ hasNoGenerics 'roundTripRep
+inspect $ (hasNoGenerics 'genericRep) { expectFail = True }
+
