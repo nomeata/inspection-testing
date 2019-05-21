a595bb17d2855ca49602438c39778a9464621259
Sandy Maguire <sandy@sandymaguire.me>
2019-05-21 00:06:28 -0400

    Give a negative example

--- examples/MutualRecursion.hs
+++ examples/MutualRecursion.hs
@@ -2,6 +2,7 @@
 {-# OPTIONS_GHC -fplugin Test.Inspection.Plugin #-}
 module MutualRecursion where
 
+import System.Exit
 import Test.Inspection
 
 inf = go0
@@ -14,34 +15,20 @@ inf' = go0
     go1 = 'b' : go2
     go0 = 'a' : go1
     go2 = 'c' : go0
-
-inspect $ 'inf === 'inf'
-
-inf2 = go0
-  where
-    go0 = 'a' : go1
-    go1 = 'b' : go2
-    go2 = 'c' : go0
 inf2' = go0
   where
     go1 = 'b' : go2
     go2 = 'c' : go0
     go0 = 'a' : go1
-
-inspect $ 'inf2 === 'inf2'
-
-inf3 = go0
-  where
-    go0 = 'a' : go1
-    go1 = 'b' : go2
-    go2 = 'c' : go0
 inf3' = go0
   where
     go0 = 'a' : go1
     go1 = 'b' : go2
     go2 = 'c' : go0
 
-inspect $ 'inf3 === 'inf3'
+inspect $ 'inf === 'inf'
+inspect $ 'inf === 'inf2'
+inspect $ 'inf === 'inf3'
 
 letrec =
   let go0 = 'a' : go1
@@ -53,26 +40,10 @@ letrec' =
       go0 = 'a' : go1
       go2 = 'c' : go0
    in go0
-
-inspect $ 'letrec === 'letrec'
-
-letrec2 =
-  let go0 = 'a' : go1
-      go1 = 'b' : go2
-      go2 = 'c' : go0
-   in go0
 letrec2' =
   let go1 = 'b' : go2
       go2 = 'c' : go0
       go0 = 'a' : go1
-   in go0
-
-inspect $ 'letrec2 === 'letrec2'
-
-letrec3 =
-  let go0 = 'a' : go1
-      go1 = 'b' : go2
-      go2 = 'c' : go0
    in go0
 letrec3' =
   let go0 = 'a' : go1
@@ -80,6 +51,22 @@ letrec3' =
       go2 = 'c' : go0
    in go0
 
-inspect $ 'letrec3 === 'letrec3'
+inspect $ 'letrec === 'letrec'
+inspect $ 'letrec === 'letrec2'
+inspect $ 'letrec === 'letrec3'
 
-main = pure ()
+something_else =
+  let go0 = 'a' : go1
+      go1 = 'b' : go0
+      go2 = 'c' : go0
+   in go0
+
+isSuccess :: Result -> Bool
+isSuccess (Success _) = True
+isSuccess (Failure _) = False
+
+main :: IO ()
+main = do
+  if isSuccess $(inspectTest $ 'letrec === 'something_else)
+     then exitFailure  -- these things are not equal
+     else exitSuccess
